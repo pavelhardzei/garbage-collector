493061c062ca6c58e50ee389cc9514fefc83f07a
pavelhardzei <pavelgordei11@gmail.com>
2021-05-06 08:47:16 +0300

    added computing methods

---
+++ ComputingMethods/MatrixInverse/Matrix.py
@@ -0,0 +1,100 @@
+import copy
+import math
+
+
+class CustomMatrix:
+    def __init__(self, matrix: list):
+        self.__matrix = matrix
+
+    def get_matrix(self) -> list:
+        return self.__matrix
+
+    def get_matrix_dimension(self) -> int:
+        return len(self.__matrix)
+
+    def set_matrix(self, matrix: list):
+        self.__matrix = matrix
+
+    def inverse(self):
+        if not self.__check_if_square_matrix():
+            raise Exception("Matrix is not square.")
+
+        size = len(self.__matrix)
+        identity_matrix = [size * [0] for _ in range(size)]
+        for i in range(size):
+            identity_matrix[i][i] = 1
+
+        for i in range(size - 1):
+            self.__choose_max(identity_matrix, i)
+            for j in range(i + 1, size):
+                self.__update_line(identity_matrix, i, j)
+
+        for l in range(size):
+            for i in range(size - 1, -1, -1):
+                summa = 0
+                for j in range(i + 1, size):
+                    summa += self.__matrix[i][j] * identity_matrix[j][l]
+                identity_matrix[i][l] -= summa
+                identity_matrix[i][l] /= self.__matrix[i][i]
+
+        self.__matrix = identity_matrix
+        return self
+
+    def solve_gauss(self, vector: list) -> list:
+        if not self.__check_if_square_matrix():
+            raise Exception("Matrix is not square.")
+        if len(self.__matrix) != len(vector):
+            raise Exception("Invalid length of vector.")
+
+        result = copy.deepcopy(vector)
+        matrix = copy.deepcopy(self.__matrix)
+        size = len(self.__matrix)
+
+        for i in range(size - 1):
+            self.__choose_max(result, i)
+            for j in range(i + 1, size):
+                self.__update_line(vector=result, base_line_index=i, row=j)
+
+        for i in range(size - 1, -1, -1):
+            summa = 0
+            for j in range(i + 1, size):
+                summa += self.__matrix[i][j] * result[j]
+            result[i] -= summa
+            result[i] /= self.__matrix[i][i]
+
+        self.__matrix = matrix
+        return result
+
+    def __check_if_square_matrix(self) -> bool:
+        for line in self.__matrix:
+            if len(line) != len(self.__matrix):
+                return False
+        return True
+
+    def __choose_max(self, matrix_or_vector: list, row: int):
+        res_row = row
+        max_element = math.fabs(self.__matrix[row][row])
+        for i in range(row + 1, len(self.__matrix)):
+            if math.fabs(self.__matrix[i][row]) > max_element:
+                max_element = math.fabs(self.__matrix[i][row])
+                res_row = i
+        self.__matrix[row], self.__matrix[res_row] = self.__matrix[res_row], self.__matrix[row]
+        matrix_or_vector[row], matrix_or_vector[res_row] = matrix_or_vector[res_row], matrix_or_vector[row]
+
+    def __update_line(self, identity_matrix: list = None, base_line_index: int = 0, row: int = 0,
+                      vector: list = None):
+        if self.__matrix[base_line_index][base_line_index] == 0:
+            raise ZeroDivisionError("Matrix is not invertible: determinant is equal to zero.")
+        l = -self.__matrix[row][base_line_index] / self.__matrix[base_line_index][base_line_index]
+        for col in range(base_line_index, len(self.__matrix)):
+            self.__matrix[row][col] += l * self.__matrix[base_line_index][col]
+            if identity_matrix is not None:
+                identity_matrix[row][col] += l * identity_matrix[base_line_index][col]
+        if vector is not None:
+            vector[row] += l * vector[base_line_index]
+
+    def __str__(self):
+        lines = ''
+        for line in self.__matrix:
+            lines += str([round(x, 2) for x in line]) + "\n"
+        return lines
---
+++ ComputingMethods/MatrixInverse/main.py
@@ -0,0 +1,118 @@
+import copy
+from resources import first_matrix, second_matrix, third_matrix, y_axis_for_slae, y_axis_for_inversion
+from Matrix import CustomMatrix
+import sympy
+import numpy
+import time
+import random
+import matplotlib.pyplot as plt
+
+
+def compute_plot_for_slae() -> list:
+    x = [50 * i for i in range(1, 18)]
+    y = []
+    for dimension in x:
+        try:
+            matrix = CustomMatrix(generate_matrix(dimension))
+            vector = generate_vector(dimension)
+            start = time.time()
+            matrix.solve_gauss(vector)
+            end = time.time()
+            y.append(end - start)
+        except ZeroDivisionError as e:
+            print(e)
+    return y
+
+
+def plot_for_slae():
+    x = [50 * i for i in range(1, 18)]
+    plt.scatter(x, y_axis_for_slae, color="green")
+    plt.xlabel("matrix dimension")
+    plt.ylabel("solving time")
+    plt.show()
+
+
+def compute_plot_for_inversion() -> list:
+    x = [50 * i for i in range(1, 13)]
+    y = []
+    for index in range(len(x)):
+        try:
+            matrix = CustomMatrix(generate_matrix(x[index]))
+            start = time.time()
+            matrix.inverse()
+            end = time.time()
+            y.append(end - start)
+        except ZeroDivisionError as e:
+            print(e)
+    return y
+
+
+def plot_for_inversion():
+    x = [50 * i for i in range(1, 13)]
+    plt.scatter(x, y_axis_for_inversion, color="green")
+    plt.xlabel("matrix dimension")
+    plt.ylabel("inversion time")
+    plt.show()
+
+
+def generate_matrix(n: int) -> list:
+    generated_matrix = []
+    for _ in range(n):
+        generated_matrix.append([random.randint(-100, 100) for _ in range(n)])
+    return generated_matrix
+
+
+def generate_vector(n: int) -> list:
+    generated_vector = []
+    for _ in range(n):
+        generated_vector.append(random.randint(-100, 100))
+    return generated_vector
+
+
+def main():
+    # First matrix
+    matrix = CustomMatrix(copy.deepcopy(first_matrix))
+    print("\tFirst matrix:")
+    try:
+        print(matrix.inverse())
+        print("\tExact matrix:")
+        exact = sympy.Matrix(copy.deepcopy(first_matrix))
+        exact = exact.inv()
+        for line in exact.tolist():
+            print(line)
+        print("\tThe norm of the difference between the exact and approximate matrices: {}"
+              .format(numpy.linalg.norm(numpy.subtract(exact.tolist(), matrix.get_matrix()), ord=numpy.inf)))
+        print()
+    except ZeroDivisionError as e:
+        print(e)
+        print()
+
+    # Second matrix
+    print("\tSecond matrix:")
+    matrix = CustomMatrix(copy.deepcopy(second_matrix))
+    print(matrix.inverse())
+    print("\tExact matrix:")
+    exact = sympy.Matrix(copy.deepcopy(second_matrix))
+    exact = exact.inv()
+    for line in exact.tolist():
+        print(line)
+    print("\tThe norm of the difference between the exact and approximate matrices: {}"
+          .format(numpy.linalg.norm(numpy.subtract(exact.tolist(), matrix.get_matrix()), ord=numpy.inf)))
+    print()
+
+    # Third matrix
+    print("\tThird matrix:")
+    matrix = CustomMatrix(copy.deepcopy(third_matrix))
+    print(matrix.inverse())
+    print("\tExact matrix:")
+    exact = sympy.Matrix(copy.deepcopy(third_matrix))
+    exact = exact.inv()
+    for line in exact.tolist():
+        print(line)
+    print("\tThe norm of the difference between the exact and approximate matrices: {}"
+          .format(numpy.linalg.norm(numpy.subtract(exact.tolist(), matrix.get_matrix()), ord=numpy.inf)))
+    print()
+
+
+if __name__ == "__main__":
+    main()

70b2f20122d98e4460ae1f734d3702560bd6d09b
pavelhardzei <pavelgordei11@gmail.com>
2021-05-31 08:57:43 +0300

    update

--- CryptTech/DES/main.py
+++ CryptTech/DES/main.py
@@ -1,109 +1,156 @@
-from bitstring import BitArray
-from resources import EXPANSION, SBOX, PERMUTATION_TABLE
+from resources import EXPANSION, SBOX, PERMUTATION_TABLE, PC1, PC2, round_shifts
 import textwrap
+import random
 
 
-def extension(encrypting_message, extension_table):
-    extended_message = BitArray()
+def apply_PC1(pc1_table: list[int], keys_64bits: int) -> int:
+    keys_56bits = 0
+    for index in pc1_table:
+        keys_56bits = (keys_56bits << 1) | ((keys_64bits >> index) & 0b1)
+    return keys_56bits
+
+
+def split_in_half(keys_56bits: int) -> tuple[int, int]:
+    left_keys, right_keys = keys_56bits >> 28, keys_56bits & 0xfffffff
+    return left_keys, right_keys
+
+
+def circular_left_shift(bits: int, numberofbits: int) -> int:
+    temp = 0
+    for i in range(28 - numberofbits):
+        temp = (temp << 1) | 0b1
+    shiftedbits = ((bits & temp) << numberofbits) | (bits >> temp)
+    return shiftedbits
+
+
+def apply_PC2(pc2_table: list[int], keys_56bits: int) -> int:
+    keys_48bits = 0
+    for index in pc2_table:
+        keys_48bits = (keys_48bits << 1) | ((keys_56bits >> index) & 0b1)
+    return keys_48bits
+
+
+def generate_keys(key_64bits: int, round_shift: list[int]) -> list[int]:
+    round_keys = []
+    pc1_out = apply_PC1(PC1, key_64bits)
+    L0, R0 = split_in_half(pc1_out)
+    for roundnumber in range(16):
+        newL = circular_left_shift(L0, round_shift[roundnumber])
+        newR = circular_left_shift(R0, round_shift[roundnumber])
+        roundkey = apply_PC2(PC2, (newL << 28) | newR)
+        round_keys.append(roundkey)
+        L0 = newL
+        R0 = newR
+    return round_keys
+
+
+# des
+def extension(encrypting_message: int, extension_table: list[int]) -> int:
+    extended_message = 0
     for index in extension_table:
-        extended_message.bin += encrypting_message.bin[index]
+        extended_message = (extended_message << 1) | ((encrypting_message >> (31 - index)) & 0b1)
     return extended_message
 
 
-def xor(encryption_message, key):
-    return encryption_message ^ key
+def split_in_6bits(encryption_message: int) -> list[int]:
+    return [(encryption_message >> (i * 6)) & 0b111111 for i in range(7, -1, -1)]
 
 
-def split_in_6bits(encryption_message):
-    list_of_6bits = textwrap.wrap(encryption_message.bin, 6)
-    return [BitArray('0b' + x) for x in list_of_6bits]
+def get_first_and_last_bit(bits6: int) -> int:
+    return (((bits6 >> 5) & 0b1) << 1) | (bits6 & 0b1)
 
 
-def get_first_and_last_bit(bits6):
-    return BitArray('0b' + bits6.bin[0] + bits6.bin[-1])
+def get_middle_four_bit(bits6: int) -> int:
+    return (bits6 >> 1) & 0b1111
 
 
-def get_middle_four_bit(bits6):
-    return BitArray('0b' + bits6.bin[1:5])
+def sbox_lookup(sbox_number: int, first_last: int, middle4: int) -> int:
+    return SBOX[sbox_number][first_last][middle4]
 
 
-def sbox_lookup(sbox_number, first_last: BitArray, middle4: BitArray):
-    first_last.prepend(BitArray('0b000000'))
-    middle4.prepend(BitArray('0b0000'))
-
-    d_first_last = int.from_bytes(first_last.bytes, 'big')
-    d_middle = int.from_bytes(middle4.bytes, "big")
-
-    sbox_value = SBOX[sbox_number][d_first_last][d_middle]
-    return BitArray(uint=sbox_value, length=4)
-
-
-def permutation(encryption_message, permutation_table):
-    cipher_text = BitArray()
+def permutation(encryption_message: int, permutation_table) -> int:
+    cipher_text = 0
     for index in permutation_table:
-        cipher_text.bin += encryption_message.bin[index]
+        cipher_text = (cipher_text << 1) | (encryption_message >> (31 - index)) & 0b1
     return cipher_text
 
 
-def round_function(encrypting_message, key):
-    result = BitArray()
+def round_function(encrypting_message: int, key: int) -> int:
+    result = 0
     encrypting_message = extension(encrypting_message, EXPANSION)
-    encrypting_message = xor(encrypting_message, key)
+    encrypting_message = encrypting_message ^ key
     bits6list = split_in_6bits(encrypting_message)
     for index, bits6 in enumerate(bits6list):
         first_last = get_first_and_last_bit(bits6)
         middle4 = get_middle_four_bit(bits6)
-        result.append(sbox_lookup(index, first_last, middle4))
+        result = (result << 4) | sbox_lookup(index, first_last, middle4)
     return permutation(result, PERMUTATION_TABLE)
 
 
-def encrypt(encrypting_block, key):
-    left_part = BitArray(encrypting_block[:32])
-    right_part = BitArray(encrypting_block[32:64])
+def encrypt(encrypting_block: int, sub_keys: list[int]) -> int:
+    left_part = encrypting_block >> 32
+    right_part = encrypting_block & 0xffffffff
 
-    for i in range(3):
+    for i in range(16):
         temp = right_part
-        right_part = xor(left_part, round_function(right_part, key))
+        right_part = left_part ^ round_function(right_part, sub_keys[i])
         left_part = temp
 
-    left_part.append(right_part)
-    return left_part
+    left_part, right_part = right_part, left_part
+
+    return (left_part << 32) | right_part
+
+
+def des(plain_text: list[bytes], sub_keys) -> list[bytes]:
+    while len(plain_text[-1]) != 8:
+        plain_text[-1] += random.randint(0, 255).to_bytes(1, 'big')
+
+    # sub_keys = generate_keys(int.from_bytes(key, 'big'), round_shifts)
+    # print(sub_keys)
+    # sub_keys = [199003930139312] * 16
+
+    result: list[bytes] = []
+    for block in plain_text:
+        block = int.from_bytes(block, 'big')
+        result.append(encrypt(block, sub_keys).to_bytes(8, 'big'))
+    return result
 
 
 def main():
-    # with open("encrypting.bin", "wb") as file:
-    #     # file.write(b'keykeyhell') the same
-    #     file.write(b'\x6b\x65\x79\x6b\x65\x79\x68\x65\x6c\x6c\x62\x61\x6c\x6c')
+    with open("encrypting.bin", "wb") as file:
+        file.write(b'keykey78hellballgooddays')
 
     with open("encrypting.bin", "rb") as file:
-        content = BitArray(file.readline())
-        if len(content) < 112:
-            print("Invalid input")
-            return
-        key = content[:48]
-        encryption_message = content[48:112]
+        key = file.read(8)
+        encryption_message = []
+        while True:
+            message_block = file.read(8)
+            if message_block == b'':
+                break
+            encryption_message.append(message_block)
 
-    encrypted = encrypt(encryption_message, key)
-    print("Encrypting message: {}".format(encryption_message.bytes))
+    sub_keys = generate_keys(int.from_bytes(key, 'big'), round_shifts)
+    encrypted = des(encryption_message, sub_keys)
+    print("Encrypting message: {}".format(encryption_message))
     print("After encrypting: {}".format(encrypted))
 
-    # with open("decrypting.bin", "wb") as file:
-    #     file.write(key.bytes)
-    #     file.write(encrypted.bytes)
+    with open("decrypting.bin", "wb") as file:
+        file.write(key)
+        for block in encrypted:
+            file.write(block)
 
     with open("decrypting.bin", "rb") as file:
-        content = BitArray(file.readline())
-        if len(content) < 112:
-            print("Invalid input")
-            return
-        key = content[:48]
-        encrypted = content[48:112]
+        key = file.read(8)
+        encrypted_message = []
+        while True:
+            message_block = file.read(8)
+            if message_block == b'':
+                break
+            encrypted_message.append(message_block)
 
-    decrypting_block = encrypted[32:64] + encrypted[:32]
-    decrypting_block = encrypt(BitArray(decrypting_block), key)
-    decrypting_block = decrypting_block[32:64] + decrypting_block[:32]
-    print("Decrypting: {}".format(decrypting_block.bytes))
+    decrypted = des(encrypted_message, sub_keys[::-1])
 
+    print("Decrypting: {}".format(decrypted))
 
 if __name__ == "__main__":
     main()
